d0e451ba26fadb06ca4ccec4b7eda60865987083
Aggelos <aggelos@webtrails.gr>
2020-11-09 20:35:46 +0200

    Convert Users entity columns to constants

--- App/Entities/Users.php
+++ App/Entities/Users.php
@@ -11,17 +11,67 @@ class Users extends EntityFunctionality implements Entity
     const TABLE_NAME = 'users';
 
     /**
+     * Column id.
+     * 
+     * @var string 
+     */
+    const COLUMN_ID = 'id';
+
+    /**
+     * Column first name.
+     * 
+     * @var string 
+     */
+    const COLUMN_FIRST_NAME = 'first_name';
+
+    /**
+     * Column last name.
+     * 
+     * @var string 
+     */
+    const COLUMN_LAST_NAME = 'last_name';
+
+    /**
+     * Column email.
+     * 
+     * @var string 
+     */
+    const COLUMN_EMAIL = 'email';
+
+    /**
+     * Column user type.
+     * 
+     * @var string 
+     */
+    const COLUMN_USER_TYPE = 'user_type';
+
+    /**
+     * Column password.
+     * 
+     * @var string 
+     */
+    const COLUMN_PASSWORD = 'password';
+
+    /**
+     * Column created.
+     * 
+     * @var string 
+     */
+    const COLUMN_CREATED = 'created';
+
+    /**
      * Default user values.
      * 
      * @var array
      */
     const DEFAULT_USER_VALUES = [
-        'id' => null,
-        'first_name' => null,
-        'last_name' => null,
-        'email' => null,
-        'user_type' => null,
-        'password' => null,
+        self::COLUMN_ID => null,
+        self::COLUMN_FIRST_NAME => null,
+        self::COLUMN_LAST_NAME => null,
+        self::COLUMN_EMAIL => null,
+        self::COLUMN_USER_TYPE => null,
+        self::COLUMN_PASSWORD => null,
+        self::COLUMN_CREATED => null,
     ];
 
     /**
