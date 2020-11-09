970b325fd776a4adbb87b0c3fa91dc4c4b237274
Aggelos <aggelos@webtrails.gr>
2020-11-09 19:51:08 +0200

    Basic handler for database connection

---
+++ App/dbConnection.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Handles the connection with the database.
+ */
+class DatabaseConnection
+{
+    /**
+     * Stores the connection to the database.
+     */
+    protected $dbConnection;
+
+    /**
+     * Conctuct function of the class.
+     * 
+     * @param string $server
+     * @param string $user
+     * @param string $password
+     * @param string $database
+     */
+    public function __construct( $server, $user, $password = null, $database = null )
+    {
+        $this->createConnection( $server, $user, $password, $database );
+    }
+
+    /**
+     * Creates a connection with the database.
+     * 
+     * @param string $server
+     * @param string $user
+     * @param string $password
+     * @param string $database
+     */
+    public function createConnection( $server, $user, $password = null, $database = null )
+    {
+        $this->dbConnection = mysqli_connect( $server, $user, $password, $database );
+    }
+
+    /**
+     * Returns the current database connection.
+     */
+    public function getConnection()
+    {
+        return $this->dbConnection;
+    }
+}
