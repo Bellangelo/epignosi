1ea87b469f381aa54d5150b2097b9a399db9cd6a
Aggelos <aggelos@webtrails.gr>
2020-11-10 19:26:11 +0200

    Basic frontend file structure

---
+++ src/pages/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <div class="main">
+            <form >
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Password" />
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+    </body>
+</html>
