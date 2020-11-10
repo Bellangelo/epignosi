666ab352f44eb97dc4c9e306e6f701945a103f05
Aggelos <aggelos@webtrails.gr>
2020-11-10 20:00:39 +0200

    Login - frontend

--- src/pages/login.php
+++ src/pages/login.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
     <head>
         <title>Login</title>
+        <link href="/epignosi/css/login.css" rel="stylesheet" />
     </head>
     <body>
         <div class="main">
-            <form >
-                <input type="text" placeholder="Email" />
-                <input type="text" placeholder="Password" />
+            <form action="/epignosi/actions/login.php" method="POST">
+                <div class="title">Login</div>
+                <input type="text" placeholder="Email" required />
+                <input type="text" placeholder="Password" required />
                 <input type="submit" value="Login" />
             </form>
         </div>
