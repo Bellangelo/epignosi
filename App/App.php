d57076c534d6a6bca9f99d8e7ffc6612b33ef264
Aggelos <aggelos@webtrails.gr>
2020-11-10 21:51:14 +0200

    Use the auth functionality in the whole app

--- App/App.php
+++ App/App.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Authentication\Auth;
+
 /**
  * Main class that includes all the important files.
  */
@@ -40,11 +42,48 @@ class App
      * 
      * @var DatabaseConnection
      */
-    protected $databaseConnection;
+    protected $databaseConnectionClass;
+
+    /**
+     * Authentication class.
+     * 
+     * @var Auth
+     */
+    protected $authClass;
+
+    /**
+     * Database connection.
+     * 
+     * @var mysqli
+     */
+    protected $dbConnection;
 
     public function __construct()
     {
-        $this->databaseConnection = new DatabaseConnection( self::DB_SERVER, self::DB_USER, 
+        $this->databaseConnectionClass = new DatabaseConnection( self::DB_SERVER, self::DB_USER, 
             self::DB_PASS, self::DB_DATABASE );
+        $this->dbConnection = $this->databaseConnectionClass->getConnection();
+        $this->authClass = new Auth( $this->dbConnection );
     }
+
+    /**
+     * Returns the authentication class.
+     * 
+     * @return Auth
+     */
+    public function getAuth()
+    {
+        return $this->authClass;
+    }
+
+    /**
+     * Returns the database connection.
+     * 
+     * @return mysqli
+     */
+    public function getDatabaseConnection()
+    {
+        return $this->dbConnection;
+    }
+
 }
