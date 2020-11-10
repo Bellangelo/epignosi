2dc6b5718bd30defbce17866cefab980c0a6c3fe
Aggelos <aggelos@webtrails.gr>
2020-11-11 00:22:18 +0200

    Basic login form

--- src/actions/login.php
+++ src/actions/login.php
@@ -2,6 +2,28 @@
 
 require_once dirname( __FILE__ ) . '/../../vendor/autoload.php';
 
+use App\Entities\Users;
+
 $app = new App\App();
+
+$dbConnection = $app->getDatabaseConnection();
+$userEntity = new Users( $dbConnection );
+
+if ( !empty( @$_POST['email'] ) && !empty( @$_POST['password'] ) ) {
+    
+    $userData = [ Users::COLUMN_PASSWORD, Users::COLUMN_ID ];
+    $user = $userEntity->find( [ Users::COLUMN_EMAIL => $_POST['email'] ], 1, $userData );
+
+    if ( empty( $user ) ) {
+        // Wrong email
+    }
+    else if ( password_verify( $_POST['password'], $user[0][ Users::COLUMN_PASSWORD ] ) ) {
+        $app->getAuth()->logUser( $user[0][ Users::COLUMN_ID ] );
+    }
+    else {
+        // Wrong password
+    }
+
+}
 
 ?>
